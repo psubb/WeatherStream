007f9f393f1426d2875b4612593a0752f6eb4991
psubb <pranavsubbiah@gmail.com>
2025-03-14 20:01:58 -0700

    Implement get_weather_data

--- weather_client.py
+++ weather_client.py
@@ -4,10 +4,25 @@ import os
 
 def get_api_key():
     load_dotenv()
-    API_KEY = os.getenv("API_KEY")
+    api_key = os.getenv("API_KEY")
+    return api_key
 
-def get_weather_data(location, date1=None, date2=None):
-    pass 
+def get_weather_data(location, api_key):
+    response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/?key={api_key}&include=current")
+    status_code = response.status_code
+    
+    if status_code == 200:
+        # only take current Conditions
+        result = response.json()["currentConditions"]
+        # remove unwanted categories
+        for i in ["datetimeEpoch", "solarradition", "solarenergy", "icon", "stations", "source", "sunriseEpoch", "sunsetEpoch"]:
+            result.pop(i, None)
+        return result
+    else:
+        print(f"Failed to retrieve, error message: {status_code}")
+        return
+        
 
 if __name__ == "__main__":
-    API_KEY = get_api_key()
+    api_key = get_api_key()
+    print(get_weather_data("danville_ca", api_key))
