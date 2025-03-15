a6edd45994c8447aad7fe8e4676a36c672b1fb3a
psubb <pranavsubbiah@gmail.com>
2025-03-14 18:46:55 -0700

    Basic weather_api test

---
+++ weather_api_test.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_response():
+    response = requests.get("http://127.0.0.1:5000/weather")
+
+    status_code = response.status_code
+
+    if status_code == 200:
+        print(response.json())
+        
+    else:
+        print("Failed, error message : {status_code}")
+
+if __name__ == "__main__":
+    get_response()
