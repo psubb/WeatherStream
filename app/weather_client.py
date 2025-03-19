dce500d8c3acdeda9f70f9b3bd507d9658b7fb67
psubb <pranavsubbiah@gmail.com>
2025-03-19 14:25:37 -0700

    Amend weather_client to return just relevant information for the current day and next day as well as current conditions

--- app/weather_client.py
+++ app/weather_client.py
@@ -8,15 +8,32 @@ def get_api_key():
     return api_key
 
 def get_weather_data(location, api_key):
-    response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/?key={api_key}&include=current")
+    response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/next1days?key={api_key}")
     status_code = response.status_code
     
     if status_code == 200:
-        # only take current Conditions
-        result = response.json()["currentConditions"]
+        result = response.json()
         # remove unwanted categories
-        for i in ["datetimeEpoch", "solarradition", "solarenergy", "icon", "stations", "source", "sunriseEpoch", "sunsetEpoch"]:
+        for i in ["latitude", "queryCost", "longitude", "address", "timezone", "tzoffset", "description", "alerts", "stations"]:
             result.pop(i, None)
+        # remove unwanted categories from "days"
+        allowed_keys = ["datetime", "temp", "precip", "precipprob", "snow", "winddir", "pressure", "visibility", "cloudcover"]
+        for day in result["days"]:
+            for key in list(day):
+                if key == "hours":
+                    # filter out the hours 
+                    for hour in range(len(day[key])):
+                        for info in list(day[key][hour]):
+                            if info not in allowed_keys:
+                                day[key][hour].pop(info, None)
+                    continue
+                if key != "datetime":
+                    day.pop(key, None)
+                    
+        for key in list(result["currentConditions"].keys()):
+            if key not in allowed_keys:
+                result["currentConditions"].pop(key, None)
+        
         return result
     else:
         print(f"Failed to retrieve, error message: {status_code}")
